135dc9891ca3c838094f43fd5fb3feadedaf27e7
dawidrogowicz <rogowiczdawid@gmail.com>
2018-08-19 11:25:53 +0200

    moved joystick inputs to a class

--- joyinput.py
+++ joyinput.py
@@ -1,20 +1,35 @@
 import pyvjoy
 import time
 
-MAX_JOY_VAL = 32767
-j = pyvjoy.VJoyDevice(1)
 
+class JoyInput:
+    """Class for manipulating virtual vJoy device.
+    Args:
+        device_num (int, optional): Number of vJoy device. Defaults to 1.
+    """
 
-def centerXAxis():
-    j.data.wAxisX = int(MAX_JOY_VAL / 2)
-    j.update()
+    MAX_JOY_VAL = 32767
+    """Maximum value of inclination each axis can have."""
 
+    def __init__(self, device_num=1):
+        self.joy = pyvjoy.VJoyDevice(device_num)
 
-def setXAxis(val, hold_time=False):
-    # val ranges from -1 to 1
-    j.data.wAxisX = int((val + 1) * MAX_JOY_VAL / 2)
-    j.update()
+    def centerXAxis(self):
+        """Sets x axis to center position."""
+        self.joy.data.wAxisX = int(self.MAX_JOY_VAL / 2)
+        self.joy.update()
 
-    if hold_time:
-        time.sleep(hold_time)
-        centerXAxis()
+    def setXAxis(self, val, hold_time=False):
+        """Sets x axis to a specified value.
+
+        Args:
+            val (int, float): Value of inclination ranging from -1 to 1.
+            hold_time(int, float, optional): If `hold_time` is set,
+                value will be released after specified time in seconds.
+        """
+        self.joy.data.wAxisX = int((val + 1) * self.MAX_JOY_VAL / 2)
+        self.joy.update()
+
+        if hold_time:
+            time.sleep(hold_time)
+            self.centerXAxis()
