8a1198b9d1de90d48306604748d3781d9d8692d4
dawidrogowicz <rogowiczdawid@gmail.com>
2018-08-24 22:34:29 +0200

    evaluating from generator + reading file using keras' HDF5Matrix

--- train.py
+++ train.py
@@ -1,46 +1,35 @@
 from math import ceil
 from subprocess import call
-import h5py
 import numpy as np
 from keras.models import Sequential
+from keras.utils.io_utils import HDF5Matrix
 from keras.layers import Conv2D, LSTM, Dense, Reshape, Input
 from keras.callbacks import TensorBoard
 
 batch_size = 256
 train_split = .8
-epochs = 4
+epochs = 2
 file_name = 'data/training_data.hdf5'
 state_name = 'state'
 action_name = 'action'
 
 
-def get_data_len(train=False, test=False):
-    data_len = 0
-    try:
-        f = h5py.File(file_name, 'a')
-        assert state_name in f and action_name in f
-        assert len(f[state_name]) == len(f[action_name])
-        data_len = len(f[state_name])
-    except Exception as e:
-        print(e)
-    finally:
-        f.close()
-    if train:
-        data_len *= train_split
-    elif test:
-        data_len *= 1 - train_split
-    return data_len
+def get_sizes():
+    len = HDF5Matrix(file_name, state_name).end
+    batches = ceil(len / batch_size)
+    train_batches = ceil(batches * train_split)
+    train_len = train_batches * batch_size
+    return train_len, len, train_batches, batches
 
 
 def get_model():
     model = Sequential()
-    # size: (19, 26, 24)
     model.add(Conv2D(24, 5, strides=3, input_shape=(60, 80, 1)))
-    model.add(Conv2D(36, 5, strides=2))  # size: (8, 11, 36)
-    model.add(Conv2D(48, 3, strides=2))  # size: (3, 5, 48)
-    model.add(Conv2D(64, 3, strides=2))  # size: (1, 2, 64)
-    model.add(Conv2D(64, (1, 2)))  # size: (1, 1, 64)
-    model.add(Reshape((64, 1)))  # size: (64,)
+    model.add(Conv2D(36, 5, strides=2))
+    model.add(Conv2D(48, 3, strides=2))
+    model.add(Conv2D(64, 3, strides=2))
+    model.add(Conv2D(64, (1, 2)))
+    model.add(Reshape((64, 1)))
     model.add(LSTM(64))
     model.add(Dense(512))
     model.add(Dense(128))
@@ -50,54 +39,29 @@ def get_model():
     return model
 
 
-def read_batch(file, test=False):
-    assert state_name in file and action_name in file
-    assert len(file[state_name]) == len(file[action_name])
-    data_size = len(file[state_name])
-    start = int(data_size * train_split) if test else 0
-    end = data_size if test else int(data_size * train_split)
-    step = batch_size
+def read_batch(test=False):
+    x = HDF5Matrix(file_name, state_name)
+    y = HDF5Matrix(file_name, action_name)
+    assert x.end == y.end
+    train_len, len, *_ = get_sizes()
+    start = train_len if test else 0
+    end = len if test else train_len
     while True:
-        for i in range(start, end, step):
-            batch_end = i + step
-            if batch_end > data_size:
-                batch_end = data_size
-            states = file[state_name][i:batch_end]
-            actions = file[action_name][i:batch_end]
-            yield states, actions
+        for i in range(start, end, batch_size):
+            batch_end = len if i + batch_size > len else i + batch_size
+            yield x[i:batch_end], y[i:batch_end]
 
 
 model = get_model()
-model.summary()
+tensorboard = TensorBoard(log_dir='./log', write_graph=False)
+train_len, _, train_batches, batches = get_sizes()
 
-tensorboard = TensorBoard(log_dir='./log', write_graph=False, write_images=True)
-
-print('Training...')
-try:
-    file = h5py.File(file_name, 'a')
-    model.fit_generator(read_batch(file), epochs=epochs, steps_per_epoch=ceil(
-        get_data_len(train=True) / batch_size), callbacks=[tensorboard])
-except Exception as e:
-    print(e)
-finally:
-    file.close()
-
-test_states = []
-test_actions = []
-try:
-    f = h5py.File(file_name, 'a')
-    assert state_name in f and action_name in f
-    assert len(f[state_name]) == len(f[action_name])
-    data_len = len(f[state_name])
-    split = int(train_split * data_len)
-    test_states = f[state_name][split:]
-    test_actions = f[action_name][split:]
-except Exception as e:
-    print(e)
-finally:
-    f.close()
-
-score, mae = model.evaluate(test_states, test_actions, batch_size=batch_size)
+print('Training {} batches'.format(train_batches))
+model.fit_generator(read_batch(), epochs=epochs, steps_per_epoch=train_batches,
+                    callbacks=[tensorboard])
+print('Evaluating {} batches'.format(batches - train_batches))
+score, mae = model.evaluate_generator(read_batch(test=True),
+                                      steps=batches - train_batches)
 print('Score: {}\nMAE: {}'.format(score, mae))
 
 call(['tensorboard', '--logdir', './log/'])
